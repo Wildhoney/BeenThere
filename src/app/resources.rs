3b1d12a0e231ec75dea4584899dee1ec45f347c0
Adam Timberlake <adam.timberlake@clearscore.com>
2022-08-25 23:17:25 +0100

    Simplified function with .ok()

--- src/app/resources.rs
+++ src/app/resources.rs
@@ -3,8 +3,5 @@ const COUNTRY_URL: &str = "https://restcountries.com/v3.1/all";
 use crate::app::types::{Countries};
 
 pub async fn get_countries() -> Option<Countries> {
-    match reqwest::get(COUNTRY_URL).await.unwrap().json::<Countries>().await {
-        Ok(countries) => Some(countries),
-        _             => None
-    }
+    reqwest::get(COUNTRY_URL).await.unwrap().json::<Countries>().await.ok()
 }
