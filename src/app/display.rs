b607797f50864c7418c950ab0654b932b3bf6ccf
Adam Timberlake <adam.timberlake@clearscore.com>
2022-09-02 17:47:01 +0100

    Added improved messaging for adding and removing actions

--- src/app/display.rs
+++ src/app/display.rs
@@ -1,15 +1,16 @@
 use figlet_rs::FIGfont;
 use colored::*;
+use num_format::{Locale, ToFormattedString};
 
-use crate::app::types::Output;
+use crate::app::types::{Country, Output};
 
 pub fn render(output: Output) -> () {
     let standard_font = FIGfont::standand().unwrap();
     println!("{}", standard_font.convert("BeenThere.").unwrap());
 
     match output {
-        Output::Add(country)    => println!("Added {} {}", country.name.common, country.flag),
-        Output::Remove(country) => println!("Removed {} {}", country.name.common, country.flag),
+        Output::Add(country)    => print_added_or_removed("Added".to_string(), country),
+        Output::Remove(country) => print_added_or_removed("Removed".to_string(), country),
         Output::List(countries) => {
 
             match countries.len() {
@@ -26,14 +27,27 @@ pub fn render(output: Output) -> () {
                 }
             }
         },
-        Output::Invalid(name) => error(format!("Invalid country: {}", name.white())),
-        Output::Noop          => error("Invalid command".to_string())
+        Output::Invalid(name) => print_error(format!("Invalid country: {}", name.white())),
+        Output::Noop          => print_error("Invalid command".to_string())
     }
 
     println!("\n");
 }
 
-fn error(message: String) -> () {
+fn print_added_or_removed(prefix: String, country: Country) -> () {
+    let area = match country.area {
+        Some(area) => format!("within {} km2.", (area as usize).to_formatted_string(&Locale::en).to_string().white()),
+        None       => format!(".")
+    };
+
+    println!(
+        "{} {} {}  which has a population of {} people {}",
+        prefix, country.name.common.white(), country.flag,
+        country.population.to_formatted_string(&Locale::en).to_string().white(), area
+    );
+}
+
+fn print_error(message: String) -> () {
     println!("{}", "Oh no! An error has occurred...".bright_red().bold());
     println!("{}", message);
 }
